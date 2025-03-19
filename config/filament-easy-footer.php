e8b5c65fd02116e3a661ba7cd36a577182749c62
Ahmad-Rizki21 <mendungriski135@gmail.com>
2025-03-19 09:21:56 +0700

    Save progress before cleaning history

--- config/filament-easy-footer.php
+++ config/filament-easy-footer.php
@@ -4,7 +4,7 @@ return [
     'app_name' => env('APP_NAME', 'Filament Footer'),
     'github' => [
         'repository' => env('GITHUB_REPOSITORY', 'https://github.com/Ahmad-Rizki21/ArtacomV1'),
-        'token' => env('GITHUB_TOKEN', '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'),
+        // 'token' => env('GITHUB_TOKEN', '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'),
         'cache_ttl' => env('GITHUB_CACHE_TTL', 3600),
     ],
 ];
