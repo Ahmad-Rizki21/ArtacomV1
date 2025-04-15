d22458e59f8baeee3e91760c98f142225abf5252
Ahmad-Rizki21 <ahmad2005rizki@gmail.com>
2025-04-15 11:09:57 +0700

    Revisi Export Pelanggan

--- app/Exports/PelangganExport.php
+++ app/Exports/PelangganExport.php
@@ -14,19 +14,23 @@ use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Concerns\WithTitle;
 use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
 class PelangganExport implements FromQuery, WithHeadings, WithMapping, WithStyles, ShouldAutoSize, WithEvents, WithTitle
 {
     use Exportable;
     
     protected $location;
+    protected $brand;
     
     /**
      * @param string|null $location
+     * @param string|null $brand
      */
-    public function __construct($location = null)
+    public function __construct($location = null, $brand = null)
     {
         $this->location = $location;
+        $this->brand = $brand;
     }
     
     /**
@@ -36,11 +40,17 @@ class PelangganExport implements FromQuery, WithHeadings, WithMapping, WithStyle
     {
         $query = Pelanggan::query();
         
+        // Filter berdasarkan lokasi
         if ($this->location) {
             $query->where(function($query) {
                 $query->where('alamat', 'like', '%' . $this->location . '%')
                       ->orWhere('alamat_2', 'like', '%' . $this->location . '%');
             });
+        }
+        
+        // Filter berdasarkan brand
+        if ($this->brand) {
+            $query->where('id_brand', $this->brand);
         }
         
         return $query;
@@ -56,15 +66,22 @@ class PelangganExport implements FromQuery, WithHeadings, WithMapping, WithStyle
             'No KTP',
             'Nama',
             'Alamat',
+            'Alamat Tambahan',
+            'Tanggal Instalasi',
             'Blok',
             'Unit',
             'No Telepon',
             'Email',
-            'Alamat Tambahan',
+            'Brand',
+            'Nama Brand', // Tambahkan kolom untuk nama brand
+            'Layanan',
+            'Brand Default',
+            'Alamat Tambahan 2',
             'Tanggal Dibuat',
             'Tanggal Diupdate'
         ];
     }
+
     
     /**
      * @param mixed $row
@@ -72,18 +89,73 @@ class PelangganExport implements FromQuery, WithHeadings, WithMapping, WithStyle
      */
     public function map($row): array
     {
+        // Fungsi helper untuk format tanggal dengan aman
+        $formatDate = function($date) {
+            if (empty($date)) {
+                return null;
+            }
+            
+            // Jika sudah objek Carbon
+            if ($date instanceof Carbon) {
+                return $date->format('Y-m-d');
+            }
+            
+            // Coba parse string ke Carbon
+            try {
+                return Carbon::parse($date)->format('Y-m-d');
+            } catch (\Exception $e) {
+                return $date; // Return as is if cannot parse
+            }
+        };
+        
+        // Fungsi helper untuk format timestamp
+        $formatTimestamp = function($date) {
+            if (empty($date)) {
+                return null;
+            }
+            
+            // Jika sudah objek Carbon
+            if ($date instanceof Carbon) {
+                return $date->format('Y-m-d H:i:s');
+            }
+            
+            // Coba parse string ke Carbon
+            try {
+                return Carbon::parse($date)->format('Y-m-d H:i:s');
+            } catch (\Exception $e) {
+                return $date; // Return as is if cannot parse
+            }
+        };
+
+       // Mapping ID brand ke nama brand
+        $brandMap = [
+            'ajn-01' => 'Jakinet',
+            'ajn-02' => 'Jelantik',
+            'ajn-03' => 'Jelantik Nagrak',
+            // Tambahkan mapping brand lainnya jika ada
+        ];
+
+        // Konversi ID brand ke nama brand
+        $brandName = isset($brandMap[$row->id_brand]) ? $brandMap[$row->id_brand] : $row->id_brand;
+
         return [
             $row->id,
             $row->no_ktp,
             $row->nama,
             $row->alamat,
+            $row->alamat_2,
+            $formatDate($row->tgl_instalasi),
             $row->blok,
             $row->unit,
             $row->no_telp,
             $row->email,
+            $row->id_brand, // Tetap menampilkan ID brand
+            $brandName,     // Menambahkan nama brand
+            $row->layanan,
+            $row->brand_default,
             $row->alamat_2,
-            $row->created_at ? $row->created_at->format('Y-m-d H:i:s') : null,
-            $row->updated_at ? $row->updated_at->format('Y-m-d H:i:s') : null,
+            $formatTimestamp($row->created_at),
+            $formatTimestamp($row->updated_at),
         ];
     }
     
@@ -106,7 +178,7 @@ class PelangganExport implements FromQuery, WithHeadings, WithMapping, WithStyle
     {
         return [
             AfterSheet::class => function(AfterSheet $event) {
-                $event->sheet->getStyle('A1:K1')->applyFromArray([
+                $event->sheet->getStyle('A1:Q1')->applyFromArray([
                     'fill' => [
                         'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
                         'startColor' => [
@@ -123,6 +195,16 @@ class PelangganExport implements FromQuery, WithHeadings, WithMapping, WithStyle
      */
     public function title(): string
     {
-        return $this->location ? "Pelanggan - {$this->location}" : 'Semua Pelanggan';
+        $title = 'Semua Pelanggan';
+        
+        if ($this->location) {
+            $title = "Pelanggan - {$this->location}";
+        }
+        
+        if ($this->brand) {
+            $title .= $this->location ? " - Brand {$this->brand}" : "Pelanggan - Brand {$this->brand}";
+        }
+        
+        return $title;
     }
 }
--- app/Http/Controllers/PelangganExportController.php
+++ app/Http/Controllers/PelangganExportController.php
@@ -9,20 +9,6 @@ use Maatwebsite\Excel\Facades\Excel;
 class PelangganExportController extends Controller
 {
     /**
-     * Export pelanggan berdasarkan lokasi
-     *
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
-     */
-    public function export(Request $request)
-    {
-        $location = $request->input('location');
-        $filename = 'pelanggan' . ($location ? "-{$location}" : '') . '.xlsx';
-        
-        return Excel::download(new PelangganExport($location), $filename);
-    }
-    
-    /**
      * Tampilkan form ekspor dengan pilihan lokasi
      *
      * @return \Illuminate\View\View
@@ -41,6 +27,41 @@ class PelangganExportController extends Controller
             'Perumahan Parama Serang' => 'Perumahan Parama Serang',
         ];
         
-        return view('pelanggan.export', compact('locations'));
+        // Ambil data brand untuk filter tambahan
+        $brands = \App\Models\Pelanggan::select('id_brand')
+                    ->distinct()
+                    ->whereNotNull('id_brand')
+                    ->pluck('id_brand', 'id_brand')
+                    ->toArray();
+        
+        return view('pelanggan.export', compact('locations', 'brands'));
+    }
+    
+    /**
+     * Export pelanggan berdasarkan lokasi dan brand
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function export(Request $request)
+    {
+        $request->validate([
+            'location' => 'nullable|string',
+            'brand' => 'nullable|string',
+        ]);
+        
+        $location = $request->input('location');
+        $brand = $request->input('brand');
+        
+        $filename = 'pelanggan';
+        if ($location) {
+            $filename .= "-" . str_replace(' ', '_', strtolower($location));
+        }
+        if ($brand) {
+            $filename .= "-brand_" . str_replace(' ', '_', strtolower($brand));
+        }
+        $filename .= '-' . now()->format('Y-m-d') . '.xlsx';
+        
+        return Excel::download(new PelangganExport($location, $brand), $filename);
     }
 }
